7a4acf41675a9c9d134922279887c4df5b14f915
akemimeka <akemimeka@gmail.com>
2021-01-02 22:14:54 -0300

    Adds form tags on NewPost component

--- src/components/new-post/index.js
+++ src/components/new-post/index.js
@@ -4,13 +4,15 @@ export const NewPost = () => {
 
   newPostContainer.innerHTML = `
     <h3 class="new-post-title">Nova resenha</h3>
-    <label class="new-post-label" for="new-post-book-title">Livro</label>
-    <input type="text" required id="new-post-book-title" class="new-post-input"></input>
-    <label class="new-post-label" for="new-post-author">Autor(a)</label>
-    <input type="text" required id="new-post-author" class="new-post-input"></input>
-    <label class="new-post-label" for="new-post-review">Resenha</label>
-    <textarea id="new-post-review" required rows="5" class="new-post-input"></textarea>
-    <button id="new-post-button">Enviar</button>
+    <form class="new-post-form">
+      <label class="new-post-label" for="new-post-book-title">Livro</label>
+      <input type="text" required id="new-post-book-title" class="new-post-input"></input>
+      <label class="new-post-label" for="new-post-author">Autor(a)</label>
+      <input type="text" required id="new-post-author" class="new-post-input"></input>
+      <label class="new-post-label" for="new-post-review">Resenha</label>
+      <textarea id="new-post-review" required rows="7" class="new-post-input"></textarea>
+      <button id="new-post-button">Publicar agora</button>
+    </form>
   `;
 
   return newPostContainer;
