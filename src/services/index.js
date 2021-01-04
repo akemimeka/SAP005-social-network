bcc67d44427399641407f8bb26a3cb10bfdccac0
akemimeka <akemimeka@gmail.com>
2021-01-03 22:28:50 -0300

    Imports redirectToLogin and redirectToFeed functions, renames userLoggedIn to verifyLogin and creates an event for the logout button

--- src/services/index.js
+++ src/services/index.js
@@ -1,14 +1,32 @@
-import { userLoggedIn } from '../router.js';
+import { redirectToLogin, redirectToFeed } from '../router.js';
 
+const logoutButton = document.querySelector('#logout-btn');
 const auth = firebase.auth();
 // const firestore = firebase.firestore();
+
+const verifyLogin = () => {
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      logoutButton.classList.remove('hidden');
+      redirectToFeed();
+    } else {
+      logoutButton.classList.add('hidden');
+      redirectToLogin();
+    }
+  });
+};
 
 export const googleLogin = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
 
   auth.signInWithPopup(provider)
     .then(() => {
-      userLoggedIn();
+      verifyLogin();
     })
     .catch();
 };
+
+logoutButton.addEventListener('click', () => {
+  auth.signOut();
+  verifyLogin();
+});
