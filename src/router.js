910917fc983d7b5c25611c9cd7b37d452623c1a5
akemimeka <akemimeka@gmail.com>
2021-01-03 22:01:07 -0300

    Removes commented events and home route, changes '/' route to Login page and creates userLoggedIn function

--- src/router.js
+++ src/router.js
@@ -1,20 +1,19 @@
-import { Home } from './pages/home/index.js';
-import { Feed } from './pages/feed/index.js';
 import { onNavigate } from './utils/history.js';
+import { Feed } from './pages/feed/index.js';
 import { Register } from './pages/register/index.js';
 import { Login } from './pages/login/index.js';
 import { Review } from './pages/review/index.js';
 import { Profile } from './pages/profile/index.js';
 
 const root = document.querySelector('#root');
+const auth = firebase.auth();
 
 const routes = {
-  '/': Home,
+  '/': Login,
   '/feed': Feed,
-  '/login': Login,
   '/register': Register,
   '/review': Review,
-  '/profile': Profile
+  '/profile': Profile,
 };
 
 const renderRoute = () => {
@@ -22,20 +21,25 @@ const renderRoute = () => {
   root.appendChild(routes[window.location.pathname]());
 };
 
-// document.querySelector('#icon-nav-feed').addEventListener('click', () => {
-//   onNavigate('/feed');
-//   renderRoute();
-// });
+export const userLoggedIn = () => {
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      const logoutButton = document.querySelector('#logout-btn');
+      logoutButton.classList.remove('hidden');
 
-document.querySelector('#home').addEventListener('click', () => {
+      onNavigate('/feed');
+      renderRoute();
+    } else {
+      onNavigate('/');
+      renderRoute();
+    }
+  });
+};
+
+document.querySelector('#logout-btn').addEventListener('click', () => {
   onNavigate('/');
   renderRoute();
 });
-
-// document.querySelector('#icon-nav-profile').addEventListener('click', () => {
-//   onNavigate('/profile');
-//   renderRoute();
-// });
 
 window.addEventListener('popstate', () => renderRoute());
 window.addEventListener('load', () => renderRoute());
