effa7bd83057673b2a13093db1bfce1f1ec49609
akemimeka <akemimeka@gmail.com>
2021-01-03 22:27:14 -0300

    Removes userLoggedIn function and creates redirectToLogin and redirectToFeed functions

--- src/router.js
+++ src/router.js
@@ -6,7 +6,6 @@ import { Review } from './pages/review/index.js';
 import { Profile } from './pages/profile/index.js';
 
 const root = document.querySelector('#root');
-const auth = firebase.auth();
 
 const routes = {
   '/': Login,
@@ -21,25 +20,15 @@ const renderRoute = () => {
   root.appendChild(routes[window.location.pathname]());
 };
 
-export const userLoggedIn = () => {
-  auth.onAuthStateChanged((user) => {
-    if (user) {
-      const logoutButton = document.querySelector('#logout-btn');
-      logoutButton.classList.remove('hidden');
-
-      onNavigate('/feed');
-      renderRoute();
-    } else {
-      onNavigate('/');
-      renderRoute();
-    }
-  });
-};
-
-document.querySelector('#logout-btn').addEventListener('click', () => {
+export const redirectToLogin = () => {
   onNavigate('/');
   renderRoute();
-});
+};
+
+export const redirectToFeed = () => {
+  onNavigate('/feed');
+  renderRoute();
+};
 
 window.addEventListener('popstate', () => renderRoute());
 window.addEventListener('load', () => renderRoute());
