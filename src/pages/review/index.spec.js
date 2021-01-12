3c41ccdcf65a0cf3a515c5a884a3dcbf79dd255f
akemimeka <akemimeka@gmail.com>
2021-01-12 20:20:36 -0300

    Creates tests for Review page

--- src/pages/review/index.spec.js
+++ src/pages/review/index.spec.js
@@ -1,7 +1,18 @@
 import { Review } from './index.js';
+import { createReview } from '../../services/index.js';
 
 describe('Review', () => {
   it('should be a function', () => {
     expect(typeof Review).toBe('function');
+  });
+
+  it('loads the page', () => {
+    expect(Review()).toMatchSnapshot();
+  });
+
+  it('clicks to send review', () => {
+    const reviewPage = Review();
+    reviewPage.querySelector('#btn-review').dispatchEvent(new Event('click'));
+    expect(createReview).toHaveBeenCalled();
   });
 });
