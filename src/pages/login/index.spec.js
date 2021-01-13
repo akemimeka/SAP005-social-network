7cb868a5985e2e67ccd6ae26f3d62ba096773904
caroline costa <carolmarcionilia@gmail.com>
2021-01-13 19:40:08 -0300

    revert

--- src/pages/login/index.spec.js
+++ src/pages/login/index.spec.js
@@ -1,19 +1,7 @@
 import { Login } from './index.js';
-// import { googleLogin } from '../../services/index.js';
-// import { emailAndPasswordLogin } from '../../services/index.js';
 
 describe('Login', () => {
   it('should be a function', () => {
     expect(typeof Login).toBe('function');
   });
-
-  it('should load the page', () => {
-    expect(Login()).toMatchSnapshot();
-  });
 });
-
-// describe('emailAndPasswordLogin', () => {
-//   it('should be a function', () => {
-//     expect(typeof renderPage).toBe('function');
-//   });
-// });
