d3b3018601dc258bdff0047781b5f255591c2bb2
caroline costa <carolmarcionilia@gmail.com>
2021-01-14 15:58:29 -0300

    one hundred percent coverage

--- src/components/navbar/index.spec.js
+++ src/components/navbar/index.spec.js
@@ -1,10 +1,22 @@
 import { Navbar } from './index.js';
+import * as utils from '../../utils/history.js';
+
+utils.onNavigate = jest.fn();
 
 describe('Navbar', () => {
   it('should be a function', () => {
     expect(typeof Navbar).toBe('function');
   });
-  it('should ', () => {
-    expect(navbar).toBeCalledWith('/');
+  it('should have a button with click event to redirect user to feed page', () => {
+    Navbar().querySelector('#icon-nav-feed').dispatchEvent(new Event('click'));
+    expect(utils.onNavigate).toBeCalledWith('/feed');
+  });
+  it('should have a button with click event to redirect user to review page', () => {
+    Navbar().querySelector('#icon-nav-review').dispatchEvent(new Event('click'));
+    expect(utils.onNavigate).toBeCalledWith('/review');
+  });
+  it('should have a button with click event to redirect user to profile page', () => {
+    Navbar().querySelector('#icon-nav-profile').dispatchEvent(new Event('click'));
+    expect(utils.onNavigate).toBeCalledWith('/profile');
   });
 });
