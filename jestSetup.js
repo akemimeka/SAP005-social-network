0f5366ff7ed6f161444c2ff14f233260a85b22f1
Jessica Cavalcante <je-cavalcante@hotmail.com>
2021-01-15 17:08:03 -0300

    add jestSetup file

--- jestSetup.js
+++ jestSetup.js
@@ -1,8 +1,21 @@
 global.firebase = {
   auth: jest.fn(() => ({
-    signInWithEmailAndPassword: jest.fn(),
+    createUserWithEmailAndPassword: jest.fn(() => Promise.resolve()),
+    signInWithEmailAndPassword: jest.fn(() => Promise.resolve()),
+    signInWithPopup: jest.fn(() => Promise.resolve()),
+    signOut: jest.fn(),
     currentUser: {
       uid: 1,
     },
+  })),
+  firestore: jest.fn(() => ({
+    collection: jest.fn(),
+    doc: jest.fn(() => ({
+      onSnapshot: () => ({
+        data: () => ({
+          likes: 1,
+        }),
+      }),
+    })),
   })),
 };
