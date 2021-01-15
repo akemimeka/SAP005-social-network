8406f27c1efd36bb3acc5be141dd1fd13c521581
akemimeka <akemimeka@gmail.com>
2021-01-15 01:47:00 -0300

    Adds styling to footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,14 +1,16 @@
 export const Footer = () => {
   const githubFooter = document.createElement('footer');
-  githubFooter.setAttribute('id', 'github-footer');
+  githubFooter.classList.add('github-footer');
 
   githubFooter.innerHTML = `
-    <span>
-    <i id="github-icon" class="fab fa-github-alt"></i> Developed by 
-    <a class="link-footer" href="https://github.com/akemimeka" target="_blank" rel="noopener noreferrer">©Akemi Mitsueda</a> 
-    <a class="link-footer" href="https://github.com/CarolineSCosta" target="_blank" rel="noopener noreferrer">©CarolineCosta</a> 
-    <a class="link-footer" href="https://github.com/JessicaCavalcante" target="_blank" rel="noopener noreferrer">©Jessica Cavalcante</a>
-    </span>
+    <p>
+      <i class="github-icon fab fa-github-alt"></i> Desenvolvido por
+    </p>
+    <p>
+      <a class="link-footer" href="https://github.com/akemimeka" target="_blank" rel="noopener noreferrer">Akemi Mitsueda</a>,  
+      <a class="link-footer" href="https://github.com/CarolineSCosta" target="_blank" rel="noopener noreferrer">Caroline Costa</a> e  
+      <a class="link-footer" href="https://github.com/JessicaCavalcante" target="_blank" rel="noopener noreferrer">Jessica&nbsp;Cavalcante</a>
+    </p>
   `;
   return githubFooter;
 };
