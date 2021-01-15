57cf92e2b9407c982615144e2f43c0fd8a6a69f6
akemimeka <akemimeka@gmail.com>
2021-01-14 22:08:43 -0300

    Login UI ready - first version

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -7,23 +7,21 @@ export const Login = () => {
 
   rootElement.innerHTML = `
       <img class="bookshelf-logo" src='../../img/logo-bookshelf.gif' />
-      <section>
-          <p class="enter"></p>
         <form class="form-login">
           <label class="label-login">E-mail <span class="required">*</span></label>
           <input type="email" class="input-login" id="email-login" name="email-login" required>
           <label class="label-login">Senha <span class="required">*</span></label>
           <input type="password" class="input-login" id="password-login" minlength="8" name="password-login" required>
           <button type="submit" class="btn-login" id="btn-login">Login</button>
-          <p class="alternative">ou</p>
           <button id="btn-google" class="btn-google">
             <img class="btn-google-icon" src="../../img/google-icon.svg" alt="Ícone do Google"/>
             <span class="btn-google-text">Entrar com conta Google</span>
           </button>
         </form>
-      </section>
-      <p class="sign-up-login">
-        Ainda não tem uma conta? <a href="#" id="sign-up-login">Registrar-se</a>
+      <p class="sign-up-question">
+        Ainda não tem uma conta?
+        <br>
+        <a href="#" id="sign-up-login" class="sign-up-link">Cadastre-se!</a>
       </p>
   `;
   const loginButton = rootElement.querySelector('#btn-login');
