7d7c2073ab89215a91b56736db5a25c993f74d45
Jessica Cavalcante <je-cavalcante@hotmail.com>
2021-01-15 01:15:23 -0300

    fix profile

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -5,19 +5,22 @@ export const Profile = () => {
   const profileBody = document.createElement('div');
   const profileHeader = document.createElement('header');
   const profileBodyNewReview = document.createElement('div');
+  const user = firebase.auth().currentUser;
   profileBodyNewReview.className = 'review-container';
   profileHeader.className = 'profile-header';
+  console.log(user);
 
   profileHeader.innerHTML = `
     <div id="profile-banner">IMG-BANNER</div>
     <div class="profile-user-info">
-      <h2 id="profile-user-name">Samara Tokki</h2>
+      <h2 id="profile-user-name">${user.displayName}</h2>
+      <img class="profile-user-avatar" src="${user.photoURL || '../../img/default_user_icon.jpg'}">
       <figure class="profile-favorite-quote">
         <blockquote id="profile-quote-text">
-          Não confio em pessoas que não se amam e mesmo assim dizem "eu te amo". Eis o que diz um provérbio africano: tome cuidado quando uma pessoa nua lhe oferece uma camisa.
+        Minha liberdade é escrever. A palavra é o meu domínio sobre o mundo.
         </blockquote>
         <figcaption id="profile-quote-author">
-          &mdash; Maya Angelou
+          &mdash; Clarice Lispector
         </figcaption>
       </figure>
     </div>
